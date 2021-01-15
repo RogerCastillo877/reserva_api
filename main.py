b9d96de798a1782898aec364da451328a66939ec
RogerCastillo877 <roger.castillo877@gmail.com>
2021-01-15 09:55:05 -0500

    Correccón main.py

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ origins = ["http://localhost.tiangolo.com",
             "https://localhost.tiangolo.com",
             "http://localhost",
             "http://localhost:8080",
-            "https://reserva-app-tosko.herokuapp.com"
+            "https://reserva-app-tosko.herokuapp.com/"
             ]
 
 api.add_middleware(CORSMiddleware,
